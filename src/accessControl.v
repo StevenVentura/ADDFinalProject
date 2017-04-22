31cdff7e1d5b7f14d12b10da294f0711d02dd7fa
Steven Ventura <8stevenventura@gmail.com>
2017-04-22 17:08:04 -0500

    ayy

--- src/accessControl.v
+++ src/accessControl.v
@@ -16,6 +16,11 @@ module accessControl (userIDfoundFlag, loadButton_s, PASSWORD, passInput, clk, r
 	reg[2:0] state;
 	reg isFlagRed;
 	reg[1:0] attemptCnt;
+	
+	//stevencode
+	always @(posedge clk)
+	accessFlag = 1'b1;//yes access
+	/*
 
 	parameter	BIT0	= 3'b000,				// 7 States
 			BIT1	= 3'b001,
@@ -127,5 +132,5 @@ module accessControl (userIDfoundFlag, loadButton_s, PASSWORD, passInput, clk, r
 				end
 			endcase
 		end
-	end
+	end*/
 endmodule
